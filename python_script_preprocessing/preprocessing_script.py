b41b980889113595d1f7f9b5cc43af696d8b0d56
Murali <murali@auraauro.com>
2021-04-23 20:55:02 +0530

    rename folder name without spaces

--- python_script_preprocessing/preprocessing_script.py
+++ python_script_preprocessing/preprocessing_script.py
@@ -5,7 +5,7 @@ Created on Thu Apr 22 22:47:44 2021
 @author: mural
 """
 import re
-f = open("D:\Arithmethic-parser-\python_preprocessing_script\myfile.txt", "r")
+f = open("D:\Arithmethic-parser-\python_script_preprocessing\myfileinputtypes.txt", "r")
 for x in f:
   print("************************************")
   print(x)
@@ -31,4 +31,27 @@ for x in f:
     eqn= eqn.replace('.(','.')
     eqn= eqn.replace('()','')
     print(f'sub:{eqn}')
+    
+    
+import xlwt
+from xlwt import Workbook
+  
+# Workbook is created
+wb = Workbook()
+  
+# add_sheet is used to create sheet.
+sheet1 = wb.add_sheet('Sheet 1')
+  
+sheet1.write(1, 0, 'ISBT DEHRADUN')
+sheet1.write(2, 0, 'SHASTRADHARA')
+sheet1.write(3, 0, 'CLEMEN TOWN')
+sheet1.write(4, 0, 'RAJPUR ROAD')
+sheet1.write(5, 0, 'CLOCK TOWER')
+sheet1.write(0, 1, 'ISBT DEHRADUN')
+sheet1.write(0, 2, 'SHASTRADHARA')
+sheet1.write(0, 3, 'CLEMEN TOWN')
+sheet1.write(0, 4, 'RAJPUR ROAD')
+sheet1.write(0, 5, 'CLOCK TOWER')
+  
+wb.save('xlwt example.xls')
    
