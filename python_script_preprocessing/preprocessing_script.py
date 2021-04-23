b362c62885bab528c4e9c952730ad44b884524eb
Murali <murali@auraauro.com>
2021-04-23 22:45:19 +0530

    read old excel and write to new excel with preprocessed exp

--- python_script_preprocessing/preprocessing_script.py
+++ python_script_preprocessing/preprocessing_script.py
@@ -4,19 +4,29 @@ Created on Thu Apr 22 22:47:44 2021
 
 @author: mural
 """
+import xlwt
+from xlwt import Workbook
+import xlrd
+from xlutils.copy import copy
 import re
-f = open("D:\Arithmethic-parser-\python_script_preprocessing\myfileinputtypes.txt", "r")
-for x in f:
-  print("************************************")
-  print(x)
-  x = re.sub(r"hz||Hz", "", x)
-  if bool(re.findall(r"[yzafEYZ]", x)):
-    pass
+
+# Give the location of the file
+loc = ("D:/Arithmethic-parser-/python_script_preprocessing/Inputtypesfilesspreadsheet.xlsx")
+ 
+# To open Workbook
+old_excel = xlrd.open_workbook(loc)
+new_excel = copy(old_excel)
+sheet = new_excel.get_sheet(0)
+
+file = open("D:\Arithmethic-parser-\python_script_preprocessing\myfileinputtypes.txt", "r")
+for idx,line in enumerate(file,1):
+  line = re.sub(r"hz||Hz", "", line)
+  if bool(re.findall(r"[yzafEYZ]", line)):
+    sheet.write(idx,1,'INVALID')
   else:
-    opr = re.findall(r'[*+-/]',x)
-    var = re.split(r"[*+-/]", x)
+    opr = re.findall(r'[*+-/]',line)
+    var = re.split(r"[*+-/]", line)
     s = ""
-    # print(f'var:{var},opt:{opr}')
     if opr != [] or opr != ['.'] :
         for i in range (len(opr)):
             if opr[i] == '.':
@@ -25,33 +35,14 @@ for x in f:
                 s=s+var[i]+")"+opr[i]
             else:
                 s= s+"("+var[i]+")"+opr[i]
-    eqn = s+"("+var[-1][:-1]+")"
-    eqn = eqn.replace('e)-(','e-')
-    eqn= eqn.replace('.(','.')
-    eqn= eqn.replace('.(','.')
-    eqn= eqn.replace('()','')
-    print(f'sub:{eqn}')
-    
-    
-import xlwt
-from xlwt import Workbook
-  
-# Workbook is created
-wb = Workbook()
-  
-# add_sheet is used to create sheet.
-sheet1 = wb.add_sheet('Sheet 1')
-  
-sheet1.write(1, 0, 'ISBT DEHRADUN')
-sheet1.write(2, 0, 'SHASTRADHARA')
-sheet1.write(3, 0, 'CLEMEN TOWN')
-sheet1.write(4, 0, 'RAJPUR ROAD')
-sheet1.write(5, 0, 'CLOCK TOWER')
-sheet1.write(0, 1, 'ISBT DEHRADUN')
-sheet1.write(0, 2, 'SHASTRADHARA')
-sheet1.write(0, 3, 'CLEMEN TOWN')
-sheet1.write(0, 4, 'RAJPUR ROAD')
-sheet1.write(0, 5, 'CLOCK TOWER')
-  
-wb.save('xlwt example.xls')
-   
+    exp = s+"("+var[-1][:-1]+")"
+    exp = exp.replace('e)-(','e-')
+    exp= exp.replace('.(','.')
+    exp= exp.replace('.(','.')
+    exp= exp.replace('()','')
+      
+    sheet.write(idx,1,exp)
+
+      
+new_excel.save('D:/Arithmethic-parser-/python_script_preprocessing/new_fileName.xls')
+       
