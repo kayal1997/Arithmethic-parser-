bff58d68d9d058ef6dfb4993ff7b5c1b9d302841
Murali <murali@auraauro.com>
2021-04-26 12:41:06 +0530

    change 1M to 1*M

--- python_script_preprocessing/preprocessing_script.py
+++ python_script_preprocessing/preprocessing_script.py
@@ -4,11 +4,13 @@ Created on Thu Apr 22 22:47:44 2021
 
 @author: mural
 """
-import xlwt
-from xlwt import Workbook
+# import xlwt
+# from xlwt import Workbook
 import xlrd
 from xlutils.copy import copy
 import re
+import aparse
+# print(aparse.parser('(20)+(20*m)'))
 
 # Give the location of the file
 loc = ("D:/Arithmethic-parser-/python_script_preprocessing/Inputtypesfilesspreadsheet.xlsx")
@@ -21,13 +23,18 @@ sheet = new_excel.get_sheet(0)
 file = open("D:\Arithmethic-parser-\python_script_preprocessing\myfileinputtypes.txt", "r")
 for idx,line in enumerate(file,1):
   line = re.sub(r"hz||Hz", "", line)
-  if bool(re.findall(r"[yzafEYZ]", line)):
-    sheet.write(idx,1,'INVALID')
+  opr = re.findall(r'[*+-/]',line)
+  opr = [o for o in opr if o!='.']
+  var = re.split(r"[*+-/]", line)
+  var = [v.replace('\n','') if '\n' in v else v for v in var]
+  print(var)
+  # var = var.remove('\n')
+  invalid_units = [True for o in opr if line[line.index(o)+1].isalpha()]
+  if bool(re.findall(r"[yzafEYZ]", line)) or any(invalid_units) :
+    sheet.write(idx,4,'INVALID')
   else:
-    opr = re.findall(r'[*+-/]',line)
-    var = re.split(r"[*+-/]", line)
     s = ""
-    if opr != [] or opr != ['.'] :
+    if opr != []:
         for i in range (len(opr)):
             if opr[i] == '.':
                 s=s+"("+var[i]+opr[i]
@@ -40,9 +47,18 @@ for idx,line in enumerate(file,1):
     exp= exp.replace('.(','.')
     exp= exp.replace('.(','.')
     exp= exp.replace('()','')
+    replace_string = [ exp[idx:idx+2] for idx,c in enumerate(exp) if c.isnumeric() and exp[idx+1].isalpha() and not exp[idx+1]=='e']
+    # print(replace_string)    
+    for rstr in replace_string:
+        exp = exp.replace(rstr,rstr[0]+'*'+rstr[-1])
+    # print(exp)
+        
+    
       
-    sheet.write(idx,1,exp)
+    sheet.write(idx,4,exp)
+    # cparsed_op = aparse.parser(exp)
+    # sheet.write(idx,5,cparsed_op)
 
       
-new_excel.save('D:/Arithmethic-parser-/python_script_preprocessing/new_fileName.xls')
+new_excel.save('D:/Arithmethic-parser-/python_script_preprocessing/output_excel.xls')
        
