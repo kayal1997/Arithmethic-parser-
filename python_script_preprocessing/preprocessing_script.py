983115f70c5eef939869df1a404e25430bd982bf
Murali <murali@auraauro.com>
2021-04-28 07:54:01 +0530

    use  more regex

--- python_script_preprocessing/preprocessing_script.py
+++ python_script_preprocessing/preprocessing_script.py
@@ -10,8 +10,8 @@ import xlrd
 from xlutils.copy import copy
 import re
 import aparse
-# print(aparse.parser('20*m'))
-# print(aparse.parser('20e6'))
+a=aparse.parser(r'20.5*M')
+# b=aparse.parser(r'20.5*M')
 
 # location of the input excel file
 loc = ("D:/Arithmethic-parser-/python_script_preprocessing/Inputtypesfilesspreadsheet.xlsx")
@@ -31,27 +31,39 @@ file = open("D:\Arithmethic-parser-\python_script_preprocessing\myfileinputtypes
 # Replace hz/Hz by ''                 ---> Example : 20+20m
 # Find list of all operators in the given expression and store in opr.      --->   opr= ['+']
 # Split the given expression by the operators in opr and store in var.      --->   var= ['20', '20m']
+from string import ascii_letters, punctuation
+
+# Fix the column width to 10000
+for i in range(6):
+    sheet.col(i).width =9000
 
 for idx,line in enumerate(file,1):
   line = line.strip()
   line = re.sub(r"hz", "", line,flags=re.I)
-  opr = re.findall(r'[*+\-/]',line)
-  var = re.split(r'[*+\-/]', line)
+  OPER = r'[*+\-/]'
+  GOODOPERS = '+-*/\(\)'
+  BADOPERS = ''.join([k for k in punctuation if k not in GOODOPERS])
+  GOODUNITS = r'munpkMGTP'
+  BADUNITS  = ''.join([k for k in ascii_letters if k not in GOODUNITS])
+  
+  opr = re.findall(OPER,line)
+  var = re.split(OPER, line)
 
   # Check for invalid inputs(operator followed by alphabet) such as 1*M,2+m
   # Check for invalid inputs(alphabets other than valid units) such as yzafEYZ,If true write INVALID to column 4 of the excel
-  invalid_units = [True for o in opr if line[line.index(o)+1].isalpha()]
-  if bool(re.findall(r"[yzafEYZ]", line)) or any(invalid_units) :
+  # invalid_units = [True for o in opr if line[line.index(o)+1].isalpha()]
+  if bool(re.findall(r"[%s]|[%s]|([%s][%s])" % (BADOPERS,BADUNITS,OPER,ascii_letters), line)) : #or any(invalid_units) :
     sheet.write(idx,4,'INVALID')
-
-  # Recreate the expression with ( and )
-  # Replace 'e)-(' by 'e-'
+    print(f'For input={line}, output="INVALID"')
+  # Recreate the expression with ( and ) on every atom includng its units
+  # Replace 'e)-(' by 'e-' e.g. 4e-9 should be (4e-9) and not (4e)-(9)
   # Replace () by ''
+  # 4e2M==>(4e2M)
   else:
     s=""
     s ="".join([ s+"("+var[i]+")"+opr[i] if i<len(opr) else s+"("+var[i]+")" for i in range (len(opr)+1) ])
-    exp = s.replace('e)-(','e-')
-    exp= exp.replace('()','')
+    exp= s.replace('()','')
+    exp= exp.replace('e)-(','e-')
     
     # Replace 1M,2m to 1*M and 2*m.
     replace_string = [ exp[idx:idx+2] for idx,c in enumerate(exp) if c.isnumeric() and exp[idx+1].isalpha() and not exp[idx+1]=='e']
@@ -60,14 +72,12 @@ for idx,line in enumerate(file,1):
     
     # Write the expression to column 4
     sheet.write(idx,4,exp)
-    
-    # Fix the column width to 10000
-    for i in range(6):
-        sheet.col(i).width =9000
-    
+
+    exp1 =(exp + '.')[:-1]
     # Write the cparsed value to column 5
-    # cparsed_op = f'Fraction{aparse.parser(exp)})'
-    # sheet.write(idx,5,cparsed_op)
+    cparsed_op = f'{aparse.parser(str(exp1))}'
+    sheet.write(idx,5,cparsed_op)
+    print(f'For input={line}, exp={exp}, cparsed_op = {cparsed_op}')
 
 # Save the new excel in the following directory.      
 new_excel.save('D:/Arithmethic-parser-/python_script_preprocessing/output_excel.xls')
